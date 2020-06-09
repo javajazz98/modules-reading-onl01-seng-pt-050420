1805dca58d710830b5a8bc26fd19a4e04bb25958
jassmine tatum <jassmine.tatum@gmail.com>
2020-06-09 01:18:28 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,13 +1,13 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
-Class kid 
-include Dance
+
+class Kid
+  include Dance
   extend MetaDancing
-
+ 
   attr_accessor :name
-
+ 
   def initialize(name)
-    @name=name
+    @name = name
   end
-
 end
